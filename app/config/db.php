cebd5a495e2b177c2133ab2c77ef1898a154657e
GoncharovAndrey <goncharov.a@triadasite.ru>
2025-06-13 09:23:23 +0300

    change db confuguration

--- app/config/db.php
+++ app/config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=172.22.0.2;dbname=yii2_db',
+    'dsn' => 'mysql:host=172.28.0.10;dbname=yii2_db',
     'username' => 'root',
     'password' => 'root',
     'charset' => 'utf8',
